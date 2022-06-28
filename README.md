647cff021d747de18df50b590c4fafe45b9db5c6
HunnyPuns <8877103+HunnyPuns@users.noreply.github.com>
2022-06-27 20:45:07 -0500

    check_memory.ps1 seems to work. Needs moar testing, though.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nagios-windows-plugins
-My Nagios Windows Plugins project
+This project is meant to be a call to the official Nagios Plugins Project. Not to compete, but to provide a base set of plugins to monitor Windows systems.
+
+In the end, I aim to have a set of plugins that can be executed over SSH or WinRM.
---
+++ check_memory.ps1
@@ -0,0 +1,98 @@
+﻿param (
+    [Parameter(Mandatory=$true)][ValidateSet('MB', 'GB', 'TB', 'PCT')][string]$outputType,
+    [Parameter(Mandatory=$true)][ValidateSet('Used', 'Available')][string]$metric,
+    
+    [Parameter(Mandatory=$false)][int]$warning = -1,
+    [Parameter(Mandatory=$false)][int]$critical = -1
+)
+
+$exitcode = 3
+$exitMessage = 'Nothing changed the status output!'
+$memoryresult = 0
+
+function processCheck {
+    param (
+        [Parameter(Mandatory=$true)][int]$checkResult,
+        [Parameter(Mandatory=$true)][int]$warningThresh,
+        [Parameter(Mandatory=$true)][int]$criticalThresh,
+        [Parameter(Mandatory=$false)][string]$returnMessage
+    )
+
+    [array]$returnArray
+
+    if ($metric -eq 'Used') {
+        if (($criticalThresh -eq -1) -and ($warningThresh -eq -1) ) {
+
+            $returnArray = @(0, "OK: $returnMessage")
+        }
+        elseif ($checkResult -gt $criticalThresh) {
+        
+            $returnArray = @(2, "CRITICAL: $returnMessage")
+        }
+        elseif ($checkResult -le $criticalThresh -and $checkResult -gt $warningThresh) {
+        
+            $returnArray = @(1, "WARNING: $returnMessage")
+        }
+        else {
+        
+            $returnArray = @(0, "OK: $returnMessage")
+        }
+    }
+    else {
+        if (($criticalThresh -eq -1) -and ($warningThresh -eq -1) ) {
+
+            $returnArray = @(0, "OK: $returnMessage")
+        }
+        elseif ($checkResult -lt $criticalThresh) {
+        
+            $returnArray = @(2, "CRITICAL: $returnMessage")
+        }
+        elseif ($checkResult -ge $criticalThresh -and $checkResult -lt $warningThresh) {
+        
+            $returnArray = @(1, "WARNING: $returnMessage")
+        }
+        else {
+        
+            $returnArray = @(0, "OK: $returnMessage")
+        }
+    }
+
+    return $returnArray
+
+}
+
+
+
+$totalmem = 0
+foreach ($mem in (Get-CimInstance -ClassName CIM_PhysicalMemory | select Capacity).Capacity) {
+    $totalmem += $mem
+}
+#Total memory in MB
+$totalmem = [int]($totalmem / 1024) / 1024
+$memoryresult = (get-counter -counter '\Memory\Available MBytes' -computername localhost).countersamples.cookedvalue
+   
+    
+switch ($outputType) {
+    'MB' { 
+            #Nothing goes here. MB is the default.
+            }
+    'GB' { 
+            $memoryresult = $memoryresult / 1024 
+            }
+    'TB' { 
+            $memoryresult = $memoryresult / 1024 / 1024
+            }
+    'PCT'{ 
+            $memoryresult = [math]::Round(($memoryresult / $totalmem) * 100, 2)
+            }
+}
+
+$processArray = processCheck -checkResult $memoryresult `
+                             -warningThresh $warning `
+                             -criticalThresh $critical `
+                             -returnMessage "$metric memory is $memoryresult | '$metric Memory'=$memoryresult$outputType;$warning;$critical"
+$exitcode = $processArray[1]
+$exitMessage = $processArray[2]
+
+write-host $exitMessage
+exit $exitcode
